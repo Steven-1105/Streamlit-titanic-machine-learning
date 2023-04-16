012292a115a0e228ab3aca93748347bd9b4b490f
LIN Hongxiang :-) <92274001+Steven-1105@users.noreply.github.com>
2023-04-16 19:18:57 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit as st
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
 from sklearn.impute import SimpleImputer
 
 # 加载数据集
