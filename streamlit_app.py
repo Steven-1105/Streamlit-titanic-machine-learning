db7616d16a6a43e092f0dd08f39f4b87236000a9
LIN Hongxiang :-) <92274001+Steven-1105@users.noreply.github.com>
2023-04-16 19:20:33 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # 导入必要的库和数据集
 import streamlit as st
 import pandas as pd
+import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
